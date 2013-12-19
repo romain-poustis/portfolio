e5f043e25735d581f9d36882d453c787713f9e0b
ychartois <yannickchartois@wanadoo.fr>
2013-12-19 11:05:50 +0100

    miss a mean.article ref

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,3 @@
-angular.module('mean', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.route', 'mean.system', 'mean.articles']);
+angular.module('mean', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.route', 'mean.system']);
 
 angular.module('mean.system', []);
