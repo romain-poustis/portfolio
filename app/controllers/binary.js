d0177c504bb1976c89a5995709cdc0885e5506c4
ychartois <yannickchartois@wanadoo.fr>
2014-01-31 15:42:59 +0100

    comment Cloudinary POC

--- app/controllers/binary.js
+++ app/controllers/binary.js
@@ -1,13 +1,13 @@
-var cloudinary = require('cloudinary'),
-    fs = require('fs'),
-    _ = require('underscore');
-
-exports.upload = function(req, res){
-  var imageStream = fs.createReadStream( req.files.image.path, { encoding: 'binary' } );
-  var cloudStream = cloudinary.uploader.upload_stream( function() { res.redirect( req.redirect.url ); });
-
-  imageStream.on('data', cloudStream.write).on('end', cloudStream.end);
-};
+//var cloudinary = require('cloudinary'),
+//    fs = require('fs'),
+//    _ = require('underscore');
+//
+//exports.upload = function(req, res){
+//  var imageStream = fs.createReadStream( req.files.image.path, { encoding: 'binary' } );
+//  var cloudStream = cloudinary.uploader.upload_stream( function() { res.redirect( req.redirect.url ); });
+//
+//  imageStream.on('data', cloudStream.write).on('end', cloudStream.end);
+//};
 
 
 
